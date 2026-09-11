e13989df166402026279cdede2eef4da560be4b2
maratbabako-star <maratbabako@gmail.com>
2026-09-11 22:17:11 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+import sys
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+# Сюда позже вставим токен от BotFather
+TOKEN = "7250291460:AAHlx1HmKwgAt0RGm8CzJ8RdVLh2VPvVk6U"
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}! Школьный бот запущен и работает!")
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.answer("Nice!")
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
